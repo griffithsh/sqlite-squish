36cfaaf011901210926cfea2c7c986a8f6c75fdb
Huw Griffiths <huw.m.griffiths@gmail.com>
2017-04-16 19:15:16 +1000

    Implement inputDir

--- cmd/sqlite-squish/inputDir.go
+++ cmd/sqlite-squish/inputDir.go
@@ -1,7 +1,40 @@
 package main
 
-import "errors"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+)
 
 func inputDir(dir string) (string, error) {
-	return "", errors.New("inputDir not implemented")
+	info, err := os.Lstat(dir)
+	if err != nil {
+		return "", err
+	}
+	if !info.IsDir() {
+		return "", fmt.Errorf("Cannot read from non-directory %s", dir)
+	}
+
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return "", err
+	}
+	var output string
+	for _, file := range files {
+		// if file ends in ".sql" ...
+		if isSQLFile(file.Name()) {
+			contents, err := ioutil.ReadFile(dir + "/" + file.Name())
+			if err != nil {
+				return "", err
+			}
+			output = output + string(contents)
+		}
+	}
+	return output, nil
+}
+
+func isSQLFile(fileName string) bool {
+	return strings.ToUpper(filepath.Ext(fileName)) == ".SQL"
 }
