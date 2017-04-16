9d569642a5f9eeb0a2d94520afef95747921d655
Huw Griffiths <huw.m.griffiths@gmail.com>
2017-04-16 17:49:35 +1000

    Implement outputText
    This method should basically accept a directory name, and then write
    the database into it in separate files, one table at a time.

--- cmd/sqlite-squish/outputText.go
+++ cmd/sqlite-squish/outputText.go
@@ -1,12 +1,49 @@
 package main
 
 import (
-	"errors"
+	"io/ioutil"
+
+	"fmt"
+
+	"os"
 
 	"github.com/griffithsh/sqlite-squish/database"
 )
 
 func outputText(d *database.Database, dir string) error {
+	// If dir does not exist, create it.
+	info, err := os.Lstat(dir)
+	if err != nil {
+		if os.IsNotExist(err) {
+			err = os.MkdirAll(dir, 0755)
+			if err != nil {
+				return err
+			}
+			info, err = os.Lstat(dir)
+			if err != nil {
+				return err
+			}
+		} else {
+			return err
+		}
+	}
 
-	return errors.New("outputText is not implemented")
+	// Else if dir is something that isnt a directory, error out.
+	if !info.IsDir() {
+		return fmt.Errorf("cannot output to non-directory %s", dir)
+	}
+
+	tableNames, err := d.SortedTables()
+	if err != nil {
+		return err
+	}
+	for _, name := range tableNames {
+		file := d.Tables[name].String()
+		filepath := fmt.Sprintf("%s/%s.sql", info.Name(), name)
+		err = ioutil.WriteFile(filepath, []byte(file), 0644)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
