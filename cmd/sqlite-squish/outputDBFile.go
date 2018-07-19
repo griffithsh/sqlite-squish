14dc92848b533260b25dda5fea5eb3dd5b8d0594
Huw Griffiths <hgriffiths@seek.com.au>
2018-07-19 17:53:36 +1000

    Implement outputDB
    This function means that users will not need the sqlite binary on their
    PATH in order to use sqlite-squish to create sqlite databases; it's now
    a native feature of sqlite-squish.

--- cmd/sqlite-squish/outputDBFile.go
+++ cmd/sqlite-squish/outputDBFile.go
@@ -1,11 +1,24 @@
 package main
 
 import (
-	"errors"
+	"database/sql"
+	"fmt"
+	"strings"
 
 	"github.com/griffithsh/sqlite-squish/database"
+	_ "github.com/mattn/go-sqlite3"
 )
 
-func outputDBFile(d *database.Database, dir string) error {
-	return errors.New("outputDB is not implemented")
+func outputDBFile(d *database.Database, file string) error {
+	db, err := sql.Open("sqlite3", file)
+	if err != nil {
+		return fmt.Errorf("create db file: %s", err)
+	}
+	for _, s := range strings.Split(d.String(), "\n") {
+		_, err := db.Exec(s)
+		if err != nil {
+			return fmt.Errorf("execute sql %s: %s", s, err)
+		}
+	}
+	return nil
 }
